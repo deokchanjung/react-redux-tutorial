db67471439b50e35f9090f63f7345c390da13d29
deokchan jung <develoduck@gmail.com>
2019-11-06 00:19:23 +0900

    react 교육용 샘플 코드 커밋

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FilteredList from './components/FilteredList';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -7,16 +7,18 @@ import { reducers } from './redux/reducers';
 // store 생성
 const store = createStore(reducers);
 
-function App() {
-  return (
-    // Provider 컴포넌트는 추후 connect 함수를 통해 특정 컴포넌트와
-    // 그에 맞는 상태를 연결하기 위해 store를 제공한다.
-    <Provider store={store}>
-      <div className="App">
-        <FilteredList />
-      </div>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      // Provider 컴포넌트는 추후 connect 함수를 통해 특정 컴포넌트와
+      // 그에 맞는 상태를 연결하기 위해 store를 제공한다.
+      <Provider store={store}>
+        <div className="App">
+          <FilteredList />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
